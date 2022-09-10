cc4603ed6d524d7fbbc36539b4d4870a10758318
mapsam <matthews.sam@gmail.com>
2022-09-10 15:04:36 -0700

    fix images again

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Block from './Block';
 import Image from 'next/image';
 
@@ -12,31 +12,13 @@ interface ContentProps {
 }
 
 export default function Content(props: ContentProps) {
-  const [ hoverImage, setHoverImage ] = useState(false);
-
-  function toggleHover(state) {
-    if (props.imgHover) {
-      setHoverImage(state);
-    }
-  }
-
   return (
     <div className="content">
       <Block id={props.name} backgroundClass="color-light-bg">
         <h1 className="title">{props.name}</h1>
         {props.img &&
-          <div
-            className={!hoverImage ? "title-image-container" : "title-image-container hidden"}
-            onMouseEnter={() => toggleHover(true)}
-            onMouseLeave={() => toggleHover(false)}
-            onTouchStart={() => toggleHover(true)}
-            onTouchEnd={() => toggleHover(false)}>
-              <Image priority={true} src={props.img} placeholder="blur" layout="responsive" />s
-          </div>
-        }
-        {props.imgHover &&
-          <div className={hoverImage ? "title-image-container" : "title-image-container hidden"}>
-              <Image priority={true} src={props.imgHover || props.img} placeholder="blur" layout="responsive" />
+          <div className="title-image-container">
+            <Image src={props.img} placeholder="blur" layout="responsive" />
           </div>
         }
         {props.summary &&
