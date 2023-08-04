f2ea09f253ae636bb47944a91f96c52ed42ea642
mapsam <matthews.sam@gmail.com>
2023-08-04 16:43:03 -0700

    update schedule, main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,8 @@ export default function Index() {
           a place we've called home for more than a decade. We'll have great food, cold drinks, and
           enough live music to lose your socks.
         </p>
-        <p>RSVPs closed on July 1st. Between now and August 12th, be sure to view the <a href="/schedule">schedule</a> and learn more about <a href="/seattle">traveling to Seattle</a>.</p>
-        <p>Have a question? Check out the <a href="/faq">FAQ</a> page.</p>
-        <p>Warmly,<br />
+        <p>Check out the <a href="/schedule">schedule</a> and learn more about <a href="/seattle">things to do in Seattle</a>. Have a question? Check out the <a href="/faq">FAQ</a> page.</p>
+        <p>Get groovy!<br />
            Sam &amp; Taylor</p>
       </Block>
     </Content>
--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -19,11 +19,8 @@ export default function Index() {
           Location: Optimism Brewery, Capitol Hill<br />
           Address: <a href="https://goo.gl/maps/3cugYKecdhcu25WR7" target="_blank">1158 Broadway, Seattle, WA 98122</a>
         </p>
-        <p style={{ backgroundColor: '#ffffae', padding: '1rem' }} className="mono">
-          Time and location has changed!
-        </p>
         <p>
-          We will have dedicated space at <a href="https://optimismbrewing.com/" target="_blank">Optimism Brewery</a> – a frequent retreat for Taylor and her co-workers, 
+          We have dedicated space at <a href="https://optimismbrewing.com/" target="_blank">Optimism Brewery</a> – a frequent retreat for Taylor and her co-workers, 
           plus it's home to Sam's favorite restroom design. First round is on us! Kids are welcome.
         </p>
         <p>
@@ -44,7 +41,7 @@ export default function Index() {
       <Block id="AUG-12">
         <h1>Saturday, August 12<sup>th</sup> - Wedding</h1>
         <p className="location-metadata">
-          Time: 5pm - Midnight<br />
+          Time: 4:30pm - 11pm<br />
           Location: Metropolist<br />
           Address: <a href="https://goo.gl/maps/3raqone4uzQdv3dQA" target="_blank">2931 First Ave. South, Suite A, Seattle, WA</a>
         </p>
@@ -54,6 +51,15 @@ export default function Index() {
           with Eldridge Gravy and the Court Supreme,
           our favorite local funk band. Get groovy people!
         </p>
+        <ul>
+          <li>04:30pm: Doors Open</li>
+          <li>05:00pm: Ceremony</li>
+          <li>05:30pm: Cocktail hour</li>
+          <li>06:30pm: Dinner (Mexican/Cuban fusion)</li>
+          <li>07:30pm: Dessert (Mini donuts) & Photo booth</li>
+          <li>08:00pm: Live band & dancing</li>
+          <li>11:00pm: Toodaloo!</li>
+        </ul>
         <p><em>What to wear</em>: Semi-formal. Dressy-casual. Sparkly chic. Bring some fun, make it funky and make sure you can comfortably dance. The event is indoors and has A/C if it's a scorcher.</p>
         <p><em>Colors:</em> If you really want to match, here's our color scheme:</p>
         <p style={{ textAlign: 'center' }}>
@@ -69,6 +75,7 @@ export default function Index() {
             backgroundSize: '180%'
           }}></div>
         </p>
+        <p><em>Before:</em> If you're looking for a round before the wedding kicks off, <a href="https://www.seapinebrewing.com/our-roots" target="_blank">Seapine brewery</a> is just around the corner. You can also grab a cocktail at <a href="https://www.westlanddistillery.com/visit" target="_blank">Westland Distillery</a>, which shares a wall with the venue. Beer and wine will be available before and during the ceremony as well.</p>
 
       </Block>
 
@@ -82,6 +89,7 @@ export default function Index() {
         <p>
           Last chance to hug that uncle of yours. Come grab coffee and a baked good in one of Seattle's lovely parks, get some fresh air, and bid farewell to the newlyweds.
         </p>
+        <p>We'll drop a pin here once we have an established location at the park.</p>
         <p>
           If you're coming from Downtown and the hotel block, you can <a href="https://www.google.com/maps/dir/Inn+at+the+WAC,+1325+6th+Ave,+Seattle,+WA+98101/Gas+Works+Park,+North+Northlake+Way,+Seattle,+WA/@47.6281032,-122.3623272,13.36z/data=!4m14!4m13!1m5!1m1!1s0x54906ab469628b59:0xd824805a4f80772e!2m2!1d-122.3333373!2d47.6096947!1m5!1m1!1s0x54901502b25bb07f:0x1e6e55abec496196!2m2!1d-122.3343532!2d47.6456308!3e3" target="_blank">take the 62 bus line</a> (35 minutes, $2.75) or grab an Uber/Lyft.
           Parking is available at the park or in the nearby neighborhood.
